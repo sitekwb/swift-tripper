43631a41be36cb4e968a321a3b484f31e62cb7f6
Wojciech <s.wojciech9@gmail.com>
2019-06-19 11:29:38 +0200

    Initial Commit

---
+++ Tripper/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Tripper
+//
+//  Created by Wojtek on 19/06/2019.
+//  Copyright © 2019 Wojciech Sitek. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tripper/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Tripper
+//
+//  Created by Wojtek on 19/06/2019.
+//  Copyright © 2019 Wojciech Sitek. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
