b6af813f819029683788888969b406c20bc97d80
Wojciech <s.wojciech9@gmail.com>
2019-06-21 20:08:11 +0200

    Initial commit. Tab bar menu, first classes.

---
+++ Tripper/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  Tripper
+//
+//  Created by Wojtek on 21/06/2019.
+//  Copyright © 2019 Wojciech Sitek. All rights reserved.
+//
+
+import Foundation
+
+class Model{
+    
+}
---
+++ Tripper/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Tripper
+//
+//  Created by Wojtek on 21/06/2019.
+//  Copyright © 2019 Wojciech Sitek. All rights reserved.
+//
+
+import Foundation
+
+class Person{
+    var name : String
+    var email : String
+    
+    init(name n:String, email e:String){
+        name = n
+        email = e
+    }
+}
